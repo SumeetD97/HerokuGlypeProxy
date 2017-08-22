f7510b4258f5140780d9c5f0a47dcef9623e1b3e
SumeetD97 <31246693+SumeetD97@users.noreply.github.com>
2017-08-22 22:55:31 +0530

    Update settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -271,4 +271,4 @@ $CONFIG['version'] = '1.4.15';
 // Anything below this line will be preserved when the admin control panel rewrites
 // the settings. Useful for storing settings that don't/can't be changed from the control panel
 
-$adminDetails['root'] = '';
+$adminDetails['rootuser'] = '';
